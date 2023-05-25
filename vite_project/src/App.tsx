c1aafc8eb6191e9556f67d23da07d6639130e536
ssori <115159126+ssori0421@users.noreply.github.com>
2023-05-25 20:27:51 +0900

    router 설정

--- vite_project/src/App.tsx
+++ vite_project/src/App.tsx
@@ -1,9 +1,15 @@
+import { Route, Routes } from 'react-router-dom';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import ToDo from './pages/ToDo';
+
 function App() {
 	return (
-		<div>
-			todo
-			<input />
-		</div>
+		<Routes>
+			<Route path="/signup" element={<SignUp />}></Route>
+			<Route path="/signin" element={<SignIn />}></Route>
+			<Route path="/todo" element={<ToDo />}></Route>
+		</Routes>
 	);
 }
 
---
+++ vite_project/src/pages/ToDo.tsx
@@ -0,0 +1,5 @@
+const ToDo = () => {
+	return <div>todo</div>;
+};
+
+export default ToDo;
