dc257e8ca533455892463be1929ea5859e788861
ssori <115159126+ssori0421@users.noreply.github.com>
2023-05-29 20:21:51 +0900

    globalstyle을 적용하여 reset css

--- vite_project/src/main.tsx
+++ vite_project/src/main.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import { BrowserRouter } from 'react-router-dom';
+import GlobalStyle from '../styles/globalstyle.ts';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
 		<BrowserRouter>
+			<GlobalStyle />
 			<App />
 		</BrowserRouter>
 	</React.StrictMode>
