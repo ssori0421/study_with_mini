1e2003f686ca582e8e2befcee1ca74ad49d01257
ssori <115159126+ssori0421@users.noreply.github.com>
2023-05-29 20:20:55 +0900

    회원가입, 로그인 기본 퍼블리싱

--- vite_project/src/pages/SignIn.tsx
+++ vite_project/src/pages/SignIn.tsx
@@ -1,5 +1,12 @@
 const SignIn = () => {
-	return <div>signin</div>;
+	return (
+		<>
+			<div>signin</div>
+			<input data-testid="email-input" />
+			<input data-testid="password-input" />
+			<button data-testid="signin-button">로그인</button>
+		</>
+	);
 };
 
 export default SignIn;
--- vite_project/src/pages/SignUp.tsx
+++ vite_project/src/pages/SignUp.tsx
@@ -1,5 +1,12 @@
 const SignUp = () => {
-	return <div>signup</div>;
+	return(
+		<>
+			<div>signup</div>
+			<input data-testid="email-input" />
+			<input data-testid="password-input" />
+			<button data-testid="signup-button">회원가입</button>;
+		</>
+	);
 };
 
 export default SignUp;
