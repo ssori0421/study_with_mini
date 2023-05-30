f1b781902389b44772ae978c70fc1bb1a049f985
ssori <115159126+ssori0421@users.noreply.github.com>
2023-05-30 23:59:04 +0900

    로그인 페이지 정규식을 통한 유효성 검사 기능 구현 및 api 연결

--- vite_project/src/pages/SignIn.tsx
+++ vite_project/src/pages/SignIn.tsx
@@ -1,11 +1,62 @@
+import { useState } from 'react';
+import { email_regex, password_regex } from '../util/regex';
+import { useNavigate } from 'react-router-dom';
+import { postSignIn } from '../service/sign';
+
 const SignIn = () => {
+	const [email, setEmail] = useState<string>('');
+	const [password, setPassword] = useState<string>('');
+	const [isValidEmail, setIsValidEmail] = useState<boolean>(false);
+	const [isValidPassword, setIsValidPassword] = useState<boolean>(false);
+
+	const Navigate = useNavigate();
+
+	const onChangeEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const { value } = e.target;
+		setEmail(value);
+		const isValidEmail = email_regex.test(value);
+		setIsValidEmail(isValidEmail);
+		if (!isValidEmail) {
+			console.error('@를 포함해서 이메일을 작성해주세요');
+		}
+	};
+
+	const onChangePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const { value } = e.target;
+		setPassword(value);
+		const isValidPassword = password_regex.test(value);
+		setIsValidPassword(isValidPassword);
+		if (!isValidPassword) {
+			console.error('8자 이상 작성 해주세요.');
+		}
+	};
+
+	const onsubmitForm = async (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		try {
+			const body = {
+				email: email,
+				password: password,
+			};
+			await postSignIn(body);
+			Navigate('/todo');
+		} catch (error: any) {
+			const errorCode = error.response.status;
+			if (errorCode < 500 && errorCode >= 400) {
+				const { message } = error.response.data;
+				alert(message);
+			}
+			console.error('로그인 실패');
+		}
+	};
+
 	return (
-		<>
+		<form onSubmit={onsubmitForm}>
 			<div>signin</div>
-			<input data-testid="email-input" />
-			<input data-testid="password-input" />
+			<input data-testid="email-input" onChange={onChangeEmail} />
+			<input data-testid="password-input" onChange={onChangePassword} />
 			<button data-testid="signin-button">로그인</button>
-		</>
+		</form>
 	);
 };
 
