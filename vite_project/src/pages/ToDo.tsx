b882306949412395de75b9575f7d7be2226fe685
ssori <115159126+ssori0421@users.noreply.github.com>
2023-06-02 00:55:54 +0900

    투두페이지 기본 퍼블리싱 및 create api 연결

--- vite_project/src/pages/ToDo.tsx
+++ vite_project/src/pages/ToDo.tsx
@@ -1,5 +1,41 @@
+import { useState } from 'react';
+import { createTodo } from '../service/todo';
+
 const ToDo = () => {
-	return <div>todo</div>;
+	const [todo, setTodo] = useState('');
+
+	const onChangeTodo = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const { value } = e.target;
+		setTodo(value);
+	};
+
+	const onCreateTodo = async () => {
+		const body = { todo: todo };
+		await createTodo(body);
+	};
+
+	return (
+		<>
+			<input data-testid="new-todo-input" onChange={onChangeTodo} />
+			<button data-testid="new-todo-add-button" onClick={onCreateTodo}>
+				추가
+			</button>
+
+			<li>
+				<label>
+					<input type="checkbox" />
+					<span>TODO 1</span>
+				</label>
+			</li>
+
+			<li>
+				<label>
+					<input type="checkbox" />
+					<span>TODO 2</span>
+				</label>
+			</li>
+		</>
+	);
 };
 
 export default ToDo;
